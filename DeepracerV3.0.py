b30262c5072ea13212882873e2c563ed17aecd9d
Benjamin Kereopa-Yorke <99622824+Benjamin-KY@users.noreply.github.com>
2022-08-08 08:53:20 +1000

    DR version 3.0, extended params and training time

---
+++ DeepracerV3.0.py
@@ -0,0 +1,47 @@
+def reward_function(params):
+    # Example of rewarding the agent to stay inside the two borders of the track
+    
+    # Read input parameters
+    all_wheels_on_track = params['all_wheels_on_track']
+    distance_from_center = params['distance_from_center']
+    track_width = params['track_width']
+    speed = params['speed']
+    steps = params['steps']
+    progress = params['progress']
+    
+    # Give a very low reward by default
+    reward = 1e-3
+
+    # Give a high reward if no wheels go off the track and
+    # the agent is somewhere in between the track borders
+    if all_wheels_on_track and (0.5*track_width - distance_from_center) >= 0.05:
+        reward = 1.0
+    
+    # Set the speed threshold based your action space
+    SPEED_THRESHOLD = 1.0
+    
+    # Customised addition using AllWheelsOnTrack AWS1    
+    if not all_wheels_on_track:
+        # Penalize if the car goes off track
+        reward = 1e-3
+    elif speed < SPEED_THRESHOLD:
+        # Penalize if the car goes too slow
+        reward = 0.5
+    else:
+        # High reward if the car stays on track and goes fast
+        reward = 1.0
+        
+# Customised addition using Steps AWS2
+    
+ # Total num of steps we want the car to finish the lap, it will vary depends on the track length
+    TOTAL_NUM_STEPS = 300
+
+    # Initialize the reward with typical value
+    reward = 1.0
+
+    # Give additional reward if the car pass every 100 steps faster than expected
+    if (steps % 100) == 0 and progress > (steps / TOTAL_NUM_STEPS) * 100 :
+        reward += 10.0
+
+    # Always return a float value
+    return float(reward)
